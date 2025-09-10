f69ee6dbce024613e2590c31ac891f974e6f0128
Sila Bulut <silab8547@gmail.com>
2025-09-10 11:46:38 +0300

    SP-8

--- src/main/java/com/Bebka_Spring_2/controller/UserController.java
+++ src/main/java/com/Bebka_Spring_2/controller/UserController.java
@@ -3,6 +3,7 @@ package com.Bebka_Spring_2.controller;
 import com.Bebka_Spring_2.dto.request.UserRequest;
 import com.Bebka_Spring_2.dto.response.UserResponse;
 import com.Bebka_Spring_2.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,8 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public UserResponse createUser(@RequestBody UserRequest request) {
+    public UserResponse createUser( @Valid @RequestBody UserRequest request) {
+
         return userService.createUser(request);
     }
 
--- src/main/java/com/Bebka_Spring_2/dto/request/UserRequest.java
+++ src/main/java/com/Bebka_Spring_2/dto/request/UserRequest.java
@@ -1,6 +1,9 @@
 package com.Bebka_Spring_2.dto.request;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +12,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRequest {
+
+    @NotBlank(message = "İsim boş olamaz")
+    @Size(min = 2, message = "İsim en az 2 karakter olmalı")
     private String name;
+
+    @NotBlank(message = "Email boş olamaz")
+    @Email(message = "Geçerli bir email adresi giriniz")
     private String email;
 }
